05d314ce949df7b12066bf042c0ba1d6e8c2eaed
GPTDevChaynik <fox.saygav@gmail.com>
2026-02-04 17:50:00 +0200

    main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Fade-in animation on scroll
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('.fade-in').forEach(el => {
+  observer.observe(el);
+});
